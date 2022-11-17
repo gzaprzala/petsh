638eb05b6529494b396aa81668a2f01b38000844
gzaprzala <zaprzala.grzegorz@gmail.com>
2022-11-17 21:14:33 +0100

    added view for search page

--- index.php
+++ index.php
@@ -8,5 +8,6 @@ $path = parse_url($path, PHP_URL_PATH);
 Routing::get('index', 'DefaultController');
 Routing::get('login', 'DefaultController');
 Routing::get('register', 'DefaultController');
+Routing::get('search', 'DefaultController');
 
 Routing::run($path);
--- src/controllers/DefaultController.php
+++ src/controllers/DefaultController.php
@@ -14,4 +14,8 @@ class DefaultController extends AppController {
   public function register() {
     $this->render('register');
   }
+
+  public function search() {
+    $this->render('search');
+  }
 }
