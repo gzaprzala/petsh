610a298486fe873b81893efc6fd65704f251fb21
gzaprzala <zaprzala.grzegorz@gmail.com>
2022-12-01 18:50:35 +0100

    added some visual changes to nav bar and chat view

--- public/views/components/navigation.php
+++ public/views/components/navigation.php
@@ -1,11 +1,11 @@
 <div class="container-navigation">
-  <a href="search">
+  <a href="search" class="a-centered">
     <img src="public/img/lens-mobile.svg" alt="lens" class="container-navigation-lens">
   </a>
-  <a href="profile">
+  <a href="profile" class="a-centered">
     <img src="public/img/profile-mobile.svg" alt="profile" class="container-navigation-profile">
   </a>
-  <a href="chat">
+  <a href="chat" class="a-centered">
     <img src="public/img/chat-mobile.svg" alt="chat" class="container-navigation-chat">
   </a>
   <img src="public/img/dark-mode.svg" alt="logo" class="container-navigation-darkmode">
