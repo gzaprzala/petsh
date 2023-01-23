2a26e01785d245c4eba20b90498a15de28d05d8e
gzaprzala <zaprzala.grzegorz@gmail.com>
2023-01-23 05:59:42 +0100

    modified chatRefresh to use fetchAPI

--- public/js/chatRefresh.js
+++ public/js/chatRefresh.js
@@ -2,23 +2,17 @@ const receiver = document.querySelector("[name='receiver_id']");
 const chat = document.querySelector(".chat-container-main-right-messages");
 
 setInterval(() => {
-  let xhr = new XMLHttpRequest();
-  xhr.open("GET", `/chat?room=${receiver.value}`, true);
-  xhr.onload = () => {
-    if (xhr.readyState === XMLHttpRequest.DONE) {
-      if (xhr.status === 200) {
-        let data = xhr.response;
+  fetch(`/chat?room=${receiver.value}`)
+    .then(response => response.text())
+    .then(data => {
         let parser = new DOMParser();
         let doc = parser.parseFromString(data, "text/html");
         let specificDiv = doc.querySelector(".chat-container-main-right-messages");
         chat.innerHTML = specificDiv.innerHTML;
-        updateScroll();
-      }
-    }
-  }
-
-  xhr.send();
+    })
+    .catch(error => console.error(error));
 }, 2000);
+
 
 function updateScroll(){
   var element = document.getElementById("chat");
