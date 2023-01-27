2b86ce60804a347b4a43667f513f77bb510ab6f5
gzaprzala <zaprzala.grzegorz@gmail.com>
2023-01-27 21:42:03 +0100

    changed method of displaying search cards

--- public/views/search.php
+++ public/views/search.php
@@ -16,38 +16,35 @@
   <div class="search-container">
     <?php include "public/views/components/navigation.php" ?>
     <div class="search-container-cards">
-      <img src="public/img/dislike.svg" alt="dislike" class="search-container-cards-dislike">
-      <div class="search-container-cards-card-parent">
-        <?php foreach ($searchCards as $searchCard) : ?>
-          <div class="search-container-cards-card">
-            <div class="search-container-cards-card-photos">
-              <img class="search-container-cards-card-photos-person" src="public/uploads/<?= $searchCard->getPersonPhoto(); ?>" alt="photo">
-              <img class="search-container-cards-card-photos-animal" src="public/uploads/<?= $searchCard->getPersonAnimalPhoto(); ?>" alt="photo">
+      <?php foreach ($searchCards as $searchCard) : ?>
+        <div class="search-container-cards-card">
+          <img class="search-container-cards-card-photos-person" src="public/uploads/<?= $searchCard->getPersonPhoto(); ?>" alt="photo">
+          <img class="search-container-cards-card-photos-animal" src="public/uploads/<?= $searchCard->getPersonAnimalPhoto(); ?>" alt="photo">
+          <div class="search-container-cards-card-description">
+            <div class="search-container-cards-card-description-person">
+              <img src="public/img/person-avatar.svg" alt="person-icon" class="search-container-cards-card-description-person-icon">
+              <span class="search-container-cards-card-description-person-name"><?= $searchCard->getPersonName(); ?></span>
+              <span class="search-container-cards-card-description-person-age"><?= $searchCard->getPersonAge(); ?></span>
             </div>
-            <div class="search-container-cards-card-description">
-              <div class="search-container-cards-card-description-person">
-                <img src="public/img/person-avatar.svg" alt="person-icon" class="search-container-cards-card-description-person-icon">
-                <span class="search-container-cards-card-description-person-name"><?= $searchCard->getPersonName(); ?></span>
-                <span class="search-container-cards-card-description-person-age"><?= $searchCard->getPersonAge(); ?></span>
-              </div>
-              <div class="search-container-cards-card-description-animal">
-                <img src="public/img/animal-avatar.svg" alt="animal-icon" class="search-container-cards-card-description-animal-icon">
-                <span class="search-container-cards-card-description-animal-name"><?= $searchCard->getPersonAnimalName(); ?></span>
-                <span class="search-container-cards-card-description-animal-age"><?= $searchCard->getPersonAnimalAge(); ?></span>
-              </div>
-              <div class="search-container-cards-card-description-city">
-                <img src="public/img/pin.svg" alt="pin" class="search-container-cards-card-description-city-icon">
-                <span class="search-container-cards-card-description-city-name"><?= $searchCard->getPersonCity(); ?></span>
-              </div>
+            <div class="search-container-cards-card-description-animal">
+              <img src="public/img/animal-avatar.svg" alt="animal-icon" class="search-container-cards-card-description-animal-icon">
+              <span class="search-container-cards-card-description-animal-name"><?= $searchCard->getPersonAnimalName(); ?></span>
+              <span class="search-container-cards-card-description-animal-age"><?= $searchCard->getPersonAnimalAge(); ?></span>
+            </div>
+            <div class="search-container-cards-card-description-city">
+              <img src="public/img/pin.svg" alt="pin" class="search-container-cards-card-description-city-icon">
+              <span class="search-container-cards-card-description-city-name"><?= $searchCard->getPersonCity(); ?></span>
             </div>
           </div>
-        <?php endforeach; ?>
-      </div>
-      <img src="public/img/like.svg" alt="like" class="search-container-cards-like">
-      <div class="search-container-cards-mobile-likes">
-        <img src="public/img/dislike.svg" alt="dislike" class="search-container-cards-mobile-likes-dislike">
-        <img src="public/img/like.svg" alt="like" class="search-container-cards-mobile-likes-like">
-      </div>
+          <div class="search-container-cards-card-reactions">
+            <img src="public/img/dislike.svg" alt="dislike" class="search-container-cards-dislike">
+            <a href="/chat?room=<?= $searchCard->getId(); ?>">
+              <img src="public/img/chat-reaction.svg" alt="chat" class="search-container-cards-chat">
+            </a>
+            <img src="public/img/like.svg" alt="like" class="search-container-cards-like">
+          </div>
+        </div>
+      <?php endforeach; ?>
     </div>
   </div>
 </body>
