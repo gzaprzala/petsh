02a6785e4b7818a2f96294bb202f7deb3992e071
gzaprzala <zaprzala.grzegorz@gmail.com>
2023-01-27 22:41:39 +0100

    added favicon

--- public/views/register.php
+++ public/views/register.php
@@ -5,6 +5,7 @@
   <link rel="stylesheet" type="text/css" href="public/css/views/register.css" />
   <link href="https://fonts.googleapis.com/css?family=Cairo" rel="stylesheet" />
   <link href="https://fonts.cdnfonts.com/css/bluetea" rel="stylesheet" />
+  <link rel="icon" type="image/x-icon" href="public/img/favicon.ico">
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
